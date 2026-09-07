a9033480daa8e0faf5ad248fd01b8fa999acaa04
henify67 <okekeifeoma537@gmail.com>
2026-09-07 12:24:55 +0100

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,8 @@
+const name = "Okeke Ifeoma";
+const age = 16;
+const favoriteLanguage = "JavaScript";
+
+console.log("Name:", name);
+console.log("Age:", age);
+console.log("Favorite programming language:", favoriteLanguage);
+console.log(`Hi, I'm ${name}. I am ${age} years old and I love ${favoriteLanguage}.`);
